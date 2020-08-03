5f128277edf3e301fde36765fafb5b0de6ef4958
KazuDante89 <karlemmanueldorvil@gmail.com>
2020-08-03 08:23:27 -0400

    build v0.0.3 	modified:   arch/arm64/configs/lavender-perf_defconfig 	modified:   drivers/cpufreq/qcom-cpufreq.c 	modified:   kernel/sched/fair.c

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -124,7 +124,7 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 	/* The old rate needs time to settle before it can be changed again */
 	delta_us = ktime_us_delta(ktime_get_boottime(), udata->last_update);
 	if (delta_us < 10000)
-		usleep_range(10000 - delta_us, 11000 - delta_us);
+		usleep_range(200000 - delta_us, 220000 - delta_us);
 	udata->last_update = ktime_get_boottime();
 
 	ret = set_cpu_freq(policy, table[index].frequency,
--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -77,13 +77,13 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling
  * Minimal preemption granularity for CPU-bound tasks:
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity = 1250000ULL;
-unsigned int normalized_sysctl_sched_min_granularity = 1250000ULL;
+ unsigned int sysctl_sched_min_granularity = 1000000ULL;
+ unsigned int normalized_sysctl_sched_min_granularity = 1000000ULL;
 
 /*
  * is kept at sysctl_sched_latency / sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 8;
+static unsigned int sched_nr_latency = 10;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
@@ -99,10 +99,10 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
  * and reduces their over-scheduling. Synchronous workloads will still
  * have immediate wakeup/sleep latencies.
  */
-unsigned int sysctl_sched_wakeup_granularity = 1000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity = 1000000UL;
+ unsigned int sysctl_sched_wakeup_granularity = 2000000UL;
+ unsigned int normalized_sysctl_sched_wakeup_granularity = 2000000UL;
 
-const_debug unsigned int sysctl_sched_migration_cost = 1000000UL;
+const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
 
 /*
  * The exponential sliding  window over which load is averaged for shares
@@ -7467,7 +7467,7 @@ static bool yield_to_task_fair(struct rq *rq, struct task_struct *p, bool preemp
  *
  * The adjacency matrix of the resulting graph is given by:
  *
- *             log_2 n     
+ *             log_2 n
  *   A_i,j = \Union     (i % 2^k == 0) && i / 2^(k+1) == j / 2^(k+1)  (6)
  *             k = 0
  *
@@ -7513,7 +7513,7 @@ static bool yield_to_task_fair(struct rq *rq, struct task_struct *p, bool preemp
  *
  * [XXX write more on how we solve this.. _after_ merging pjt's patches that
  *      rewrite all of this once again.]
- */ 
+ */
 
 static unsigned long __read_mostly max_load_balance_interval = HZ/10;
 
@@ -8244,7 +8244,7 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 		/*
 		 * !SD_OVERLAP domains can assume that child groups
 		 * span the current group.
-		 */ 
+		 */
 
 		group = child->groups;
 		do {
